a29aad16082e61391fe20ebf24bf328b7026c710
Gabriel <gabr.cr1910@gmail.com>
2022-06-16 22:08:28 -0600

    ajidnioanu

--- Juego.java
+++ Juego.java
@@ -10,6 +10,8 @@ public class Juego {
 
     public Juego(){
         this.jugadores = new Vector<Jugador>();
+        this.jugadores.add(new Jugador("Player 1"));
+        this.jugadores.add(new Jugador("Player 2"));
         this.pila = new Stack<Carta>();
         this.comodines = new Stack<Carta>();
         this.baraja = new Vector<Carta>();
@@ -34,7 +36,7 @@ public class Juego {
     public void crear_baraja() {
         for (int i = 1; i < 14; i += 1) {
             baraja.add(new Carta (i, "corazon"));
-            baraja.add(new Carta (i, "espada"));
+            baraja.add(new Carta (i, "pica"));
             baraja.add(new Carta (i, "trebol"));
             baraja.add(new Carta (i, "rombo"));
         }
@@ -49,6 +51,37 @@ public class Juego {
             this.pila.push(this.baraja.elementAt(index));
         }
         System.out.println(barajadas);
+    }
+
+    public void repartir_cartas() {
+        for (int i = 0; i < jugadores.size(); i += 1) {
+            for (int j = 0; j < 7; j += 1) {
+                jugadores.elementAt(i).recibir_carta(this.pila.pop());
+            }
+        }
+    }
+
+    public void turno(Jugador jugador, int num) {
+       /*Carta c;
+        if (num == 0) {
+            c = this.pila.pop();
+            jugador.recibir_carta(c);
+            System.out.println("Has recibido: "+ c.get_numero() + " | " + c.get_tipo());
+        }
+        else {
+            System.out.println("De donde deseas comer una carta?");
+        }*/
+    }
+
+    public void jugar() {
+       /*  this.crear_baraja();
+        this.crear_pila();
+        this.repartir_cartas();
+        int turno = this.numero_aleatorio(0, 2);
+        boolean hay_ganador = false;
+        while (!hay_ganador) {
+
+        }*/
     }
 
 }
--- Jugador.java
+++ Jugador.java
@@ -11,9 +11,26 @@ public class Jugador {
         this.cartas = new Vector<Carta>();
     }
 
-    /*public void comer_cartas(Stack<Carta> pila) {
-        cartas.add(pila.pop(i,));
-    }*/
+    public void recibir_carta(Carta c){
+        this.cartas.add(c);
+    }
 
-    
+    public Carta desechar_carta(int index) {
+        return this.cartas.elementAt(index);
+    }
+
+    public String get_nombre() {
+        return this.nombre;
+    }
+
+    public Vector<Carta> get_cartas() {
+        return this.cartas;
+    }
+
+    public void ver_cartas() {
+        System.out.println("LISTADO DE CARTAS: ");
+        for (int i = 0; i < cartas.size(); i += 1) {
+            System.out.println(i+". Numero: " + cartas.elementAt(i).get_numero() + " | " + "Tipo: " + cartas.elementAt(i).get_tipo());
+        }
+    }
 }
