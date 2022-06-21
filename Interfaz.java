e8c419ead838238ea723bbca62e027c80f730ce5
GREIVIN JOSE ARCE NAVARRO <greivin.arcenavarro@ucr.ac.cr>
2022-06-21 10:07:49 -0600

    se añade interfaz

--- Interfaz.java
+++ Interfaz.java
@@ -3,13 +3,17 @@ import java.util.Vector;
 import java.awt.Image;
 import java.awt.event.*;
 
-public class Interfaz implements ActionListener {
+public class Interfaz /*implements ActionListener*/ {
     private JFrame frame;
-    private Vector<JButton> botones;
+    private Vector<JButton> mano_cartas;
+    private JButton carta_secreta;
+    private JButton comodin;
     
     public Interfaz(){
         this.frame = new JFrame();
-        this.botones = new Vector<JButton>();
+        this.mano_cartas = new Vector<JButton>();
+        this.carta_secreta = new JButton();
+        this.comodin = new JButton();
     }
 
     public JButton crear_boton_carta(int x, int y, int width, int height, String imagen) {
@@ -22,26 +26,21 @@ public class Interfaz implements ActionListener {
         return boton;
     }
 
-    public Vector<JButton> pantalla_principal(Vector<String> mano, String tope_desechadas) {
-        Vector<JButton> mano_cartas = new Vector<JButton>();
-        JButton carta_secreta;
-        JButton carta_comestible;
-        JFrame frame = new JFrame(); 
-
+    public void pantalla_principal(Vector<String> mano, String tope_desechadas) {
         // Se crean botones de la mano de cartas
         int x = 600;
         for (int i = 0; i < mano.size(); i += 1) {
             JButton boton = crear_boton_carta(x, 100, 127, 200, mano.elementAt(i));
-            mano_cartas.add(boton);
-            frame.add(boton);
+            this.mano_cartas.add(boton);
+            this.frame.add(boton);
             x += 127;
         }
 
-        carta_secreta = crear_boton_carta(250, 100, 127, 200, "imagenes/carta.png");
+        this.carta_secreta = crear_boton_carta(250, 100, 127, 200, "imagenes/carta.png");
         frame.add(carta_secreta);
 
-        carta_comestible = crear_boton_carta(250, 450, 127, 200, tope_desechadas);
-        frame.add(carta_comestible);
+        this.comodin = crear_boton_carta(250, 450, 127, 200, tope_desechadas);
+        frame.add(comodin);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920,1080);
@@ -50,14 +49,29 @@ public class Interfaz implements ActionListener {
     
         frame.setVisible(true);
 
-        return mano_cartas;
     }
 
-    public void actionPerformed(ActionEvent e){
+    public void reset() {
+        frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+    }
+
+    public Vector<JButton> get_mano_cartas() {
+        return this.mano_cartas;
+    }
+
+    public JButton get_comodin() {
+        return this.comodin;
+    }
+
+    public JButton get_carta_secreta() {
+        return this.carta_secreta;
+    }
+
+    /* public void actionPerformed(ActionEvent e){
         if (e) {
 
         }
-    }
+    }*/
 
 
 }
