3cde8d4ee9cc1c1228f48077e1b83350ba894221
christopherperez <christopher.perez.cr@gmail.com>
2022-07-01 18:12:38 -0600

    Agregando el metodo que escribe en el archivo

--- GuardaPartidas.java
+++ GuardaPartidas.java
@@ -1,5 +1,19 @@
-import org.json.*; 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Stack; 
 
 public class GuardaPartidas {
-    
+
+    public static void escribir_en_archivo(Jugador jugador1, Jugador jugador2, Stack<Carta> mazo, Stack<Carta> comodines) {
+        
+        String str = ""; 
+        try {
+            FileWriter archivo_con_partida = new FileWriter("Estado_de_partida_anterior.txt");
+            archivo_con_partida.write(str);
+            archivo_con_partida.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
